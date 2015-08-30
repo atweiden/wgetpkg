f17741938c58ac1870d5f672b7419cd3d7e3d63c
Andy Weidenbaum <archbaum@gmail.com>
2015-08-30 09:11:21 -0700

    s/aur4/aur

--- wgetpkg.pl
+++ wgetpkg.pl
@@ -5,5 +5,5 @@ use 5.022;
 while (defined(my $pkg = shift(@ARGV)))
 {
     chomp($pkg);
-    system("wget", "https://aur4.archlinux.org/cgit/aur.git/snapshot/$pkg.tar.gz");
+    system("wget", "https://aur.archlinux.org/cgit/aur.git/snapshot/$pkg.tar.gz");
 }
